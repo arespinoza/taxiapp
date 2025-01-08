a1ca57b963f574ebba2f002ea4322bef02d8198c
arespinoza <alfredo_espi@hotmail.com>
2025-01-08 18:44:49 -0300

    primeros retoques

--- taxifront/src/app/app.component.ts
+++ taxifront/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
